69efc407a8bae707b1c476885bdfb28a5c3390c4
Juan Gabriel Tavárez López <jtavarez@intellisys.com.do>
2022-01-21 23:59:32 -0400

    fix: get cloudinary image url

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Player } from '../../interfaces/main.interface';
-import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-card',
@@ -9,10 +8,10 @@ import { environment } from '../../../environments/environment';
 })
 export class CardComponent implements OnInit {
   @Input() player!: Player;
-  BACKEND_URL: string = environment.BACKEND_URL;
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
 }
